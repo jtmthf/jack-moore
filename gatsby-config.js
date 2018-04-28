4636bd4e7667e4164af76b356556549a99cef2e4
Jack Moore <jtmthf@mst.edu>
2018-04-28 18:31:09 -0500

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Jack Moore',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi I'm Jack</h1>
+    <h2>I'm a full-stack web developer based out of Kansas City.</h2>
+  </div>
+)
+
+export default IndexPage
