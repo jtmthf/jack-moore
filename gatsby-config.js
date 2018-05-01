dc53e33153b0912f2bf61119327671f801c9e7fc
Jack Moore <jtmthf@mst.edu>
2018-04-30 23:24:57 -0500

    added ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'Jack Moore',
+    siteUrl: 'https://jackmoore.co',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-typescript'],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-118449642-1',
+      },
+    },
+  ],
+};
